02f434eef1eff48a1ce7a5c0187326d33a9822ce
Pedro Correa <pedro.nec@hotmail.com>
2024-01-14 14:35:19 -0300

    updates

--- resources/views/pages/admin/configurations/home.blade.php
+++ resources/views/pages/admin/configurations/home.blade.php
@@ -1,6 +1,6 @@
 <x-layout.admin>
     <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
-        <x-admin-header title="Configurações" :paths="['Configurações']"/>
+        <x-header-admin title="Configurações" :paths="['Configurações']"/>
         <div class="container-fluid py-4">
             <div class="row mt-4">
                 <div class="col-12">
