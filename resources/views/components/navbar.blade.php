2b5d388da3f1f845e88d27f1a43e27b1a000a589
Pedro Correa <pedro.nec@hotmail.com>
2024-01-18 13:52:43 -0300

    logo

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -5,8 +5,8 @@
             <nav
                 class="navbar navbar-expand-lg blur blur-rounded top-0 z-index-3 shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
                 <div class="container-fluid pe-0">
-                    <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 " href="../pages/dashboard.html">
-                        Logo
+                    <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 p-0" href="{{ url('/') }}">
+                        <img src="{{ asset('img/logo/logo.png') }}" class="" alt="" height="30">
                     </a>
                     <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse"
                         data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false"
